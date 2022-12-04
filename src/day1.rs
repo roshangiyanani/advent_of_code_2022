4c61f7168f88c470230435da97a91f00a3ec4a63
Roshan Giyanani <roshangiyanani@gmail.com>
2022-12-04 14:09:23 -0500

    complete day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,59 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use aoc_runner_derive::{aoc, aoc_generator};
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<Vec<u32>> {
+    let mut elves = Vec::<Vec<u32>>::new();
+    let mut calories = Vec::<u32>::new();
+
+    for line in input.lines() {
+        if line.is_empty() {
+            elves.push(calories);
+            calories = Vec::new();
+        } else {
+            calories.push(u32::from_str_radix(line, 10).expect("could not parse calories"))
+        }
+    }
+
+    if !calories.is_empty() {
+        elves.push(calories);
+    }
+
+    elves
+}
+
+#[aoc(day1, part1)]
+pub fn most_calories_held(input: &[Vec<u32>]) -> u32 {
+    input
+        .iter()
+        .map(|elf| elf.iter().sum())
+        .max()
+        .expect("no elves with food")
+}
+
+#[aoc(day1, part2)]
+pub fn calories_held_by_top_3_elves(input: &[Vec<u32>]) -> u32 {
+    let mut sums = input.iter().map(|elf| elf.iter().sum::<u32>());
+
+    let mut min_heap = BinaryHeap::with_capacity(3);
+    for _ in 0..3 {
+        min_heap.push(Reverse(sums.next().expect("must have >= 3 elves")));
+    }
+
+    while let Some(sum) = sums.next() {
+        if sum < min_heap.peek().unwrap().0 {
+            ()
+        } else {
+            min_heap.pop();
+            min_heap.push(std::cmp::Reverse(sum));
+        }
+    }
+
+    min_heap.iter().map(|r| r.0).sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
